cf58f1116505d85ebc6407d3075c02447ae4782a
Charan163 <163422577+Charan163@users.noreply.github.com>
2024-09-06 17:16:34 +0530

    Create submit.php

---
+++ PHP/submit.php
@@ -0,0 +1,30 @@
+<?php
+
+// Create connection
+$conn = new mysqli("localhost","root","","vivira_mall");
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+// Prepare and bind
+$stmt = $conn->prepare("INSERT INTO contact_form (name, email, subject, message) VALUES (?, ?, ?, ?)");
+$stmt->bind_param("ssss", $name, $email, $subject, $message);
+
+// Execute the statement
+if ($stmt->execute()) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $stmt->error;
+}
+
+// Close connections
+$stmt->close();
+$conn->close();
+?>
